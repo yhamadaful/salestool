d2a9d0cbb8ac10b736fbf6cfa0573554fa9c913b
nttr <nttr@BI-23569.local>
2017-08-23 14:55:05 +0900

    Initial Commit

---
+++ SalesToolApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SalesToolApp
+//
+//  Created by nttr on 2017/08/23.
+//  Copyright © 2017年 nttr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
